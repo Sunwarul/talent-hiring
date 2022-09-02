4470855eed2308d78f2d23bbdb916581efe66d87
Sunwarul Islam <sanoarulslm@gmail.com>
2022-09-02 12:52:10 +0600

    spa setup

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title', .' - '. config('app.name'))</title>
+        <meta name="CSRF_TOKEN" content="{{ csrf_field() }}">
     </head>
     <body class="antialiased">
+        <main id="app"></main>
 
+        @vite(['resources/app.js'])
     </body>
 </html>
