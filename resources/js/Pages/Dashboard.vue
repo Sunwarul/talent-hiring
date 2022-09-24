95efe773338b5d242d618b9fb43e7a9bedbae5d3
Sunwarul Islam <sanoarulslm@gmail.com>
2022-09-24 10:25:20 +0600

    stancl/tenancy required and composer install

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,7 +17,10 @@ import { Head } from '@inertiajs/inertia-vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+                        You're logged in! welcome here
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolor sit numquam consequuntur? Explicabo esse cupiditate molestiae dolore sit, magnam iure est quidem accusantium, nisi laborum aliquam? Commodi, suscipit tempora.
+                        </p>
                     </div>
                 </div>
             </div>
