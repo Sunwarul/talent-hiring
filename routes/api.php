5f4b801ee6c83be2a34bba93e6e761224985110e
Sunwarul Islam <sanoarulslm@gmail.com>
2022-09-30 00:57:58 +0600

    tenant routes and other settings.

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\V1\UserController;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\API\V1\UserController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::apiResources([
-    'users' => UserController::class,
-]);
+// Route::apiResources([
+//     'users' => UserController::class,
+// ]);
